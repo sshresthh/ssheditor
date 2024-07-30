0fb055450ac830ba363cb04b1f5897daedf254b2
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-31 02:30:49 +0930

    (updated) the function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function insert() {
-  let html_code = document.getElementById("html-code");
-  let css_code = document.getElementById("css-code");
-  let cssl_code = document.getElementById("javascript-code");
-  let output = document.getElementById("output");
+  let html_code = document.getElementById("html-code").value;
+  let css_code = document.getElementById("css-code").value;
+  let cssl_code = document.getElementById("javascript-code").value;
+  let output = document.getElementById("output").value;
 }
