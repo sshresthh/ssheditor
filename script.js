e3f2f54f44de5e1982317a64540b01051ac49f01
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-31 01:46:53 +0930

    (updated) declared variables

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
 function insert() {
-
+  let html_code = document.getElementById("html-code");
+  let css_code = document.getElementById("css-code");
+  let cssl_code = document.getElementById("javascript-code");
+  let output = document.getElementById("output");
 }
