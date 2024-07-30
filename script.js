ad25f372beac4d0eb31f561b2b84689c8d75dc8b
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-31 02:31:07 +0930

    (fixed) the value of output

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function insert() {
   let html_code = document.getElementById("html-code").value;
   let css_code = document.getElementById("css-code").value;
   let cssl_code = document.getElementById("javascript-code").value;
-  let output = document.getElementById("output").value;
+  let output = document.getElementById("output");
 }
