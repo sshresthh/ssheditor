4671945f14bc71e41fde32681be1429d5736615b
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-31 02:33:19 +0930

    (uodated) made the final changed

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function insert() {
   let html_code = document.getElementById("html-code").value;
   let css_code = document.getElementById("css-code").value;
-  let cssl_code = document.getElementById("javascript-code").value;
+  let javascript_code = document.getElementById("javascript-code").value;
   let output = document.getElementById("output");
 
-  output.contentDocument.body.innerHTML = html_code;
+  output.contentDocument.body.innerHTML = html_code + "<style>" + css_code + "</style>" + "<script>" + javascript_code + "</script>";
 }
