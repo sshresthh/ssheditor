dc2f59516604b880b7c7984310a6a6b37ec2f5aa
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-31 02:47:56 +0930

    (updated)

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function insert() {
+function update() {
   let html_code = document.getElementById("html-code").value;
   let css_code = document.getElementById("css-code").value;
   let javascript_code = document.getElementById("javascript-code").value;
   let output = document.getElementById("output");
 
-  output.contentDocument.body.innerHTML =
-    html_code +
-    "<style>" +
-    css_code +
-    "</style>" +
-    "<script>" +
-    javascript_code +
-    "</script>";
+  output.contentDocument.body.innerHTML = html_code + "<style>" + css_code + "</style>";
+  output.contentWindow.eval(javascript_code);
 }
+
+document.getElementById("html-code").addEventListener("input", update);
+document.getElementById("css-code").addEventListener("input", update);
+document.getElementById("javascript-code").addEventListener("input", update);
+
+update();
